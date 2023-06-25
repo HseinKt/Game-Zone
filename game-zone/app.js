f7ac365afef085b8c52e28530cb529e95bb4a283
HseinKt <hseinkteish1998@gmail.com>
2023-06-25 03:29:58 +0300

    useState and button in react native

--- game-zone/app.js
+++ game-zone/app.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import react, {useState} from 'react';
 
 export default function App() {
+  const [name, setName] = useState("Hussein");
+
+  const handleButton = () => { 
+    setName("AYA");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello there!</Text>
+      <View style={styles.boldText}>
+        <Text>Hello there! my name is {name}</Text>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Button title='click here' onPress={handleButton}/>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +31,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boldText: {
+    backgroundColor: 'yellow',
+    fontWeight: 'bold',
+    padding: 50,
+  },
+  buttonContainer: {
+    backgroundColor: 'lightblue',
+    borderBottomColor: 'black',
+    borderWidth: 2,
+    borderRadius: 20,
+    marginTop: 50,
+  }
+
 });
